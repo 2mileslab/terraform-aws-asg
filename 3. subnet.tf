8b5d6a178df1e11df3eac068564289fdf759fa42
2miles <82986758+2mileslab@users.noreply.github.com>
2022-08-02 18:28:56 +0900

    Update 3. subnet.tf

--- 3. subnet.tf
+++ 3. subnet.tf
@@ -1,19 +1,19 @@
-resource "aws_subnet" "user30-subnet1" {
-  vpc_id            = aws_vpc.user30-vpc.id
-  availability_zone = "ap-southeast-1a"
-  cidr_block        = "30.0.1.0/24"
+resource "aws_subnet" "user*-subnet1" {
+  vpc_id            = aws_vpc.user*-vpc.id
+  availability_zone = "ap-southeast-1a" # 본인이 속한 리전의 AZ을 입력합니다 ex) us-east-1a
+  cidr_block        = "*.0.1.0/24"
 
   tags  = {
-    Name = "user30-subnet1"
+    Name = "user*-subnet1"
   }
 }
 
-resource "aws_subnet" "user30-subnet2" {
-  vpc_id            = aws_vpc.user30-vpc.id
-  availability_zone = "ap-southeast-1b"
-  cidr_block        = "30.0.2.0/24"
+resource "aws_subnet" "user*-subnet2" {
+  vpc_id            = aws_vpc.user*-vpc.id
+  availability_zone = "ap-southeast-1b" # 본인이 속한 리전의 AZ을 입력합니다 ex) us-east-1b
+  cidr_block        = "*.0.2.0/24"
 
   tags  = {
-    Name = "user30-subnet2"
+    Name = "user*-subnet2"
   }
 }
