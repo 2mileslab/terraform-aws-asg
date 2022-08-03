0e2cfba0827cb663ef7d7f6e4dd97603a80309c4
2miles <82986758+2mileslab@users.noreply.github.com>
2022-08-03 18:07:18 +0900

    Update 12. alb.tf

--- 12. alb.tf
+++ 12. alb.tf
@@ -1,28 +1,28 @@
-resource "aws_alb" "user30-alb" {
-    name = "user30-alb"
+resource "aws_alb" "user*-alb" {
+    name = "user*-alb"
     internal = false
-    security_groups = [aws_security_group.user30-alb-sg.id]
+    security_groups = [aws_security_group.user*-alb-sg.id]
     subnets = [
-        aws_subnet.user30-subnet1.id,
-        aws_subnet.user30-subnet2.id
+        aws_subnet.user*-subnet1.id,
+        aws_subnet.user*-subnet2.id
     ]
     access_logs {
-        bucket = aws_s3_bucket.user30-s3.id
-        prefix = "user30-alb"
+        bucket = aws_s3_bucket.user*-s3.id
+        prefix = "user*-alb"
         enabled = true
     }
     tags = {
-        Name = "user30-alb"
+        Name = "user*-alb"
     }
     lifecycle { create_before_destroy = true }
 }
 
 resource "aws_alb_listener" "http" {
-    load_balancer_arn = aws_alb.user30-alb.arn
+    load_balancer_arn = aws_alb.user*-alb.arn
     port = "80"
     protocol = "HTTP"
     default_action {
-        target_group_arn = aws_alb_target_group.user30-alb-tg.arn
+        target_group_arn = aws_alb_target_group.user*-alb-tg.arn
         type = "forward"
     }
 }
