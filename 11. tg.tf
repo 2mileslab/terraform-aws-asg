1fc443ee6ab58c3dd1898ae641ca91f3de6170c0
2miles <82986758+2mileslab@users.noreply.github.com>
2022-08-03 18:03:50 +0900

    Update 11. tg.tf

--- 11. tg.tf
+++ 11. tg.tf
@@ -1,14 +1,13 @@
-#alb target group
-resource "aws_alb_target_group" "user30-alb-tg" {
-    name = "user30-alb-tg"
+resource "aws_alb_target_group" "user*-alb-tg" {
+    name = "user*-alb-tg"
     port = 80
     protocol = "HTTP"
-    vpc_id = aws_vpc.user30-vpc.id
+    vpc_id = aws_vpc.user*-vpc.id
     health_check {
         interval = 30
         path = "/"
         healthy_threshold = 3
         unhealthy_threshold = 3
     }
-    tags = { Name = "user30-Frontend Target Group" }
+    tags = { Name = "user*-Frontend Target Group" }
 }
