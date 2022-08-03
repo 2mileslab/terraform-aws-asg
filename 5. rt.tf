242c3d3d5234468b08328add73db9eeb05003e73
2miles <82986758+2mileslab@users.noreply.github.com>
2022-08-03 11:09:56 +0900

    Update 5. rt.tf

--- 5. rt.tf
+++ 5. rt.tf
@@ -1,23 +1,23 @@
-resource "aws_route_table" "user30-rt" {
-  vpc_id = aws_vpc.user30-vpc.id
+resource "aws_route_table" "user*-rt" {
+  vpc_id = aws_vpc.user*-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.user30-igw.id
+    gateway_id = aws_internet_gateway.user*-igw.id
   }
 
   tags = {
-    Name = "user30-rt"
+    Name = "user*-rt"
   }
 }
 
 
 resource "aws_route_table_association" "rt_subnet1" {
-  subnet_id      = aws_subnet.user30-subnet1.id
-  route_table_id = aws_route_table.user30-rt.id
+  subnet_id      = aws_subnet.user*-subnet1.id
+  route_table_id = aws_route_table.user*-rt.id
 }
 
 resource "aws_route_table_association" "rt_subnet2" {
-  subnet_id      = aws_subnet.user30-subnet2.id
-  route_table_id = aws_route_table.user30-rt.id
+  subnet_id      = aws_subnet.user*-subnet2.id
+  route_table_id = aws_route_table.user*-rt.id
 }
