fb114231267eab8f7f78294ade89890aa2f91597
2miles <82986758+2mileslab@users.noreply.github.com>
2022-08-03 16:20:37 +0900

    Update 0. variable.tf

--- 0. variable.tf
+++ 0. variable.tf
@@ -1,11 +1,11 @@
 variable "amazon_linux" {
-  	default = "ami-0e8e39877665a7c92"
+  	default = "ami-0e8e39877665a7c92" # 본인이 속한 리전의 Amazon Linux 2 AMI ID를 입력합니다
 }
 
-variable "user30_keyname" {
-  	default = "user30-key"
+variable "user*_keyname" {
+  	default = "user*-key"
 }
 
 variable "alb_account_id" {  
-	default = "114774131450"
+	default = "114774131450" # 본인이 속한 리전의 Elastic Load Balancing account ID를 입력합니다
 }
